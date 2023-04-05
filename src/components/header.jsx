82ab8d4c1ff8f2aedb49c261ce26452a15e2cde8
Nikhil Parashar <49810009+nikHIL0505@users.noreply.github.com>
2023-04-05 23:22:09 +0530

    setup project (#1)
    Co-authored-by: Nikhil Parashar <nikhilparashar@Nikhils-MacBook-Pro.local>

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <div className="header-container">
+      <div className="header-logo-container">
+        <h2>NCR Tech</h2>
+      </div>
+      <div className="header-navigation-container"></div>
+    </div>
+  );
+}
+
+export default Header;
